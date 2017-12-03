022d8c6176bbe1c12c4a21a2a2aa865c5d012b1f
Wampie Driessen <wampiedriessen@gmail.com>
2017-12-03 11:19:11 +0100

    Dag 2 oplossing (ik heb hem nog even mooier gemaakt ;)

---
+++ 2017/day02/src/main.rs
@@ -0,0 +1,47 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn divisive_numbers(v: &mut std::vec::Vec<i32>) -> i32 {
+    for i in 0..v.len()-1 {
+        for j in i+1..v.len()-1 {
+            if v[i] % v[j] == 0 {
+                return v[i] / v[j] as i32;
+            }
+            if v[j] % v[i] == 0 {
+                return v[j] / v[i] as i32;
+            }
+        }
+    }
+    return 0;
+}
+
+fn main() {
+    let f = File::open("input.txt").expect("no file?");
+    let reader = BufReader::new(f);
+
+    let mut sum: i32 = 0;
+    let mut sum2: i32 = 0;
+
+    let mut v = std::vec::Vec::with_capacity(15);
+
+    for line in reader.lines() {
+
+        for num in line.unwrap().trim().split("\t") {
+            if num == "" {
+                continue;
+            }
+            let x: i32 = num.parse().expect("No INT! :(");
+            v.push(x);
+        }
+
+        v.sort();
+
+        sum2 += divisive_numbers(&mut v);
+        sum += v[v.len()-1] - v[0];
+        v.clear();
+
+        println!("Sum: {}", sum);
+        println!("Sum2: {}", sum2);
+    }
+}
