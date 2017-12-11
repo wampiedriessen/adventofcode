4b1e25b23ead988c06d87a7cc824b8f164b42b0f
Wampie Driessen <wampiedriessen@gmail.com>
2017-12-11 17:32:40 +0100

    Verbeter dag 9, en doe de output hetzelfde als alle andere

--- 2017/day09/src/main.rs
+++ 2017/day09/src/main.rs
@@ -3,11 +3,11 @@ use std::io::BufReader;
 use std::fs::File;
 
 fn calc_score(line: String) {
-    let mut depth:u64 = 0;
-    let mut score:u64 = 0;
+    let mut depth:u32 = 0;
+    let mut score:u32 = 0;
     let mut in_garbage:bool = false;
     let mut skip:bool = false;
-    let mut garbage_count:u64 = 0;
+    let mut garbage_count:u32 = 0;
 
     for chr in line.chars() {
         if skip {
@@ -42,15 +42,16 @@ fn calc_score(line: String) {
         }
     }
 
-    println!("Score: {:?}", score);
-    println!("Garbage count: {:?}", garbage_count);
+    println!("Part 1: {:?}", score);
+    println!("Part 2: {:?}", garbage_count);
 }
 
 fn main() {
     let f = File::open("input.txt").expect("no file?");
-    let reader = BufReader::new(f);
+    let mut reader = BufReader::new(f);
 
-    let line = reader.lines().next().unwrap().unwrap();
+    let mut line:String = String::new();
+    reader.read_line(&mut line).expect("failed to read line");
 
     calc_score(line);
 }
