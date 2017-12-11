1bee251c313285e61b9d827d98ecc4c2ea54eb51
Wampie Driessen <wampiedriessen@gmail.com>
2017-12-11 16:56:07 +0100

    Dag 3 een beetje beter zo

--- 2017/day03/src/main.rs
+++ 2017/day03/src/main.rs
@@ -1,18 +1,35 @@
-fn endrace(r : i32, d : i32) {
-    println!("Right: {}", r);
-    println!("Down: {}", d);
-    println!("Sum: {}", d+r);
+fn main() {
+    let input = 361527;
+    part1(input);
+    part2(input);
 }
 
-fn main1() {
-    let input = 361527;
-    println!("input: {}", input);
+fn endrace(r : i32, d : i32) {
+    println!("Part 1: {}", d+r);
+}
 
+fn check_last(input:i32, grid: &mut [&mut [i32]], x: usize, y:usize) -> bool
+{
+    grid[x][y] = 
+        grid[x-1][y-1] +
+        grid[x][y-1] +
+        grid[x+1][y-1] +
+        grid[x-1][y] +
+        grid[x+1][y] +
+        grid[x-1][y+1] +
+        grid[x][y+1] +
+        grid[x+1][y+1];
+
+    if grid[x][y] > input {
+        println!("Part 2: {}", grid[x][y]);
+        return true;
+    }
+    return false;
+}
+
+fn part1(input: i32) {
     let base:i32 = (input as f32).sqrt() as i32;
     let kwadraat = base.pow(2);
-    
-    println!("base: {}", base);
-    println!("kwadraat: {}", kwadraat);
 
     let mut stepsdown: i32 = (base-1) / 2;
     let mut stepsright: i32 = (base-1) / 2;
@@ -39,30 +56,7 @@ fn main1() {
     endrace(stepsright.abs(), stepsdown.abs());
 }
 
-fn check_last(input:i32, grid: &mut [&mut [i32]], x: usize, y:usize) -> bool
-{
-    grid[x][y] = 
-        grid[x-1][y-1] +
-        grid[x][y-1] +
-        grid[x+1][y-1] +
-        grid[x-1][y] +
-        grid[x+1][y] +
-        grid[x-1][y+1] +
-        grid[x][y+1] +
-        grid[x+1][y+1];
-
-    println!("x: {}, y: {}, val: {}", x,y,grid[x][y]);
-
-    if grid[x][y] > input {
-        println!("DONE! {}", grid[x][y]);
-        return true;
-    }
-    return false;
-}
-
-fn main() {
-    main1();
-    let input = 361527;
+fn part2(input: i32) {
     let width = 600;
     let height = 600;
     // Base 1d array
