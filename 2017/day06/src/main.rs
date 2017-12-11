b0fbcf27aef49623057d2621735845ea29d92011
Wampie Driessen <wampiedriessen@gmail.com>
2017-12-11 16:56:44 +0100

    kleine edit dag 6

--- 2017/day06/src/main.rs
+++ 2017/day06/src/main.rs
@@ -1,3 +1,26 @@
+fn main() {
+    let mut mem_bank = [4,10,4,1,8,4,9,14,5,1,14,15,0,15,3,5];
+    let mut configurations:Vec<String> = Vec::with_capacity(10000);
+    let mut index: i32;
+
+    add_config(&mut configurations, mem_bank);
+
+    loop {
+        let start = max_index(mem_bank);
+        let distribute = mem_bank[start];
+        for i in 0..distribute {
+            mem_bank[(start + 1 + i as usize) % 16] += 1;
+        }
+        mem_bank[start] -= distribute;
+        index = add_config(&mut configurations, mem_bank);
+        if index != -1 {
+            break;
+        }
+    }
+    println!("Part 1: {:?}", configurations.len());
+    println!("Part 2: {:?}", configurations.len() as i32 - index);
+}
+
 fn max_index(v: [i32; 16]) -> usize {
     let mut ind: usize = 0;
     let mut max: i32 = 0;
@@ -18,26 +41,4 @@ fn add_config(confs: &mut Vec<String>, v: [i32; 16]) -> i32 {
     }
     confs.push(conf.clone());
     return -1;
-}
-
-fn main() {
-    let mut mem_bank = [4,10,4,1,8,4,9,14,5,1,14,15,0,15,3,5];
-    let mut configurations:Vec<String> = Vec::with_capacity(10000);
-
-    add_config(&mut configurations, mem_bank);
-
-    loop {
-        let start = max_index(mem_bank);
-        let distribute = mem_bank[start];
-        for i in 0..distribute {
-            mem_bank[(start + 1 + i as usize) % 16] += 1;
-        }
-        mem_bank[start] -= distribute;
-        let index = add_config(&mut configurations, mem_bank);
-        if index != -1 {
-            println!("Loop length: {:?}", index - configurations.len() as i32);
-            break;
-        }
-    }
-    println!("Ends at: {:?}", configurations.len());
 }
