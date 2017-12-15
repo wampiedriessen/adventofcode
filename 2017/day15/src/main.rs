9d831bb44d898e53fe8972568faa7374c123f5bf
Wampie Driessen <wampied@calculus.nl>
2017-12-15 11:11:02 +0100

    I made a struct

--- 2017/day15/src/main.rs
+++ 2017/day15/src/main.rs
@@ -1,21 +1,54 @@
-fn main() {
-    part1();
-    part2();
+#[derive(Debug, Clone, Copy)]
+struct Generator {
+    factor:u64,
+    modulo:u64,
+    latest:u64,
 }
 
-fn part1() {
+impl Generator {
+    pub fn next(&mut self) -> u64 {
+        self.latest = self.latest * self.factor % 2147483647;
+        return self.latest;
+    }
+
+    pub fn nextloop(&mut self) -> u64 {
+        loop {
+            //do:
+            self.latest = self.next();
+            //while:
+            if (self.latest % self.modulo) == 0 {
+                break;
+            }
+        }
+        return self.latest;
+    }
+}
+
+fn main() {
     let inputa = 516;
     let inputb = 190;
-    let mut preva:u64 = inputa;
-    let mut prevb:u64 = inputb;
+    let gen_a = Generator {
+        factor: 16807,
+        modulo: 4,
+        latest: inputa
+    };
+    let gen_b = Generator {
+        factor: 48271,
+        modulo: 8,
+        latest: inputb
+    };
+    part1(gen_a, gen_b);
+    part2(gen_a, gen_b);
+}
 
+fn part1(mut gen_a:Generator, mut gen_b:Generator) {
     let mut matchcount = 0;
 
     for _ in 0..40000000 {
-        preva = preva * 16807 % 2147483647;
-        prevb = prevb * 48271 % 2147483647;
+        let a = gen_a.next();
+        let b = gen_b.next();
 
-        if (preva & 0xFFFF) == (prevb & 0xFFFF) {
+        if (a & 0xFFFF) == (b & 0xFFFF) {
             matchcount += 1;
         }
     }
@@ -23,29 +56,14 @@ fn part1() {
     println!("Part 1: {:?}", matchcount);
 }
 
-fn part2() {
-    let inputa = 516;
-    let inputb = 190;
-    let mut preva:u64 = inputa;
-    let mut prevb:u64 = inputb;
-
+fn part2(mut gen_a:Generator, mut gen_b:Generator) {
     let mut matchcount = 0;
 
     for _ in 0..5000000 {
-        while {
-            //do:
-            preva = preva * 16807 % 2147483647;
-            //while:
-            (preva % 4) != 0
-        } {}
-        while {
-            //do:
-            prevb = prevb * 48271 % 2147483647;
-            //while:
-            (prevb % 8) != 0
-        } {}
+        let a = gen_a.nextloop();
+        let b = gen_b.nextloop();
 
-        if (preva & 0xFFFF) == (prevb & 0xFFFF) {
+        if (a & 0xFFFF) == (b & 0xFFFF) {
             matchcount += 1;
         }
     }
