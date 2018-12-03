7885c3e73689a4cf96fad5c63c61209b272d46f6
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2018-12-03 10:25:38 +0100

    Kleine code-duplicatie weggehaald

--- 2018/Solutions/Day3.hs
+++ 2018/Solutions/Day3.hs
@@ -9,25 +9,8 @@ import qualified Data.List as L
 -- #1 @ 1,3: 4x4
 -- left,top,width,height
 
-parse :: String -> [(Int,Int)]
+parse :: String -> (String,[(Int,Int)])
 parse claim =
-    let
-        left = read $ takeWhile (/=',') $ tail $ dropWhile (/='@') claim
-        top = read $ takeWhile (/=':') $ tail $ dropWhile (/=',') claim
-        width = read $ takeWhile (/='x') $ tail $ dropWhile (/=':') claim
-        height = read $ tail $ dropWhile (/='x') claim
-    in [(x,y) | x <- [left..(left+width-1)], y <- [top..(top+height-1)]]
-
-solveP1 :: [String] -> String
-solveP1 = show . length . filterSingles . createGridItems
-    where
-        createGridItems = L.group . L.sort . concat . map parse
-        filterSingles = filter ((>=2) . length)
-
--- || Start Part 2
-
-parse2 :: String -> (String,[(Int,Int)])
-parse2 claim =
     let
         key = tail $ takeWhile (/=' ') claim
         left = read $ takeWhile (/=',') $ tail $ dropWhile (/='@') claim
@@ -36,10 +19,19 @@ parse2 claim =
         height = read $ tail $ dropWhile (/='x') claim
     in (key,[(x,y) | x <- [left..(left+width-1)], y <- [top..(top+height-1)]])
 
+
+solveP1 :: [String] -> String
+solveP1 = show . length . filterSingles . createGridItems
+    where
+        createGridItems = L.group . L.sort . concat . map (snd . parse)
+        filterSingles = filter ((>=2) . length)
+
+-- || Start Part 2
+
 solveP2 :: [String] -> String
 solveP2 x = 
     let
-        gridItems = L.sort $ concat $ map parse x
+        gridItems = L.sort $ concat $ map (snd . parse) x
         isSingleInList grid x = 1 == (length $ filter (x==) grid)
         allSingleInList grid x = all (isSingleInList grid) x
-    in fst $ head $ filter ((allSingleInList gridItems) . snd) $ map parse2 x
+    in fst $ head $ filter ((allSingleInList gridItems) . snd) $ map parse x
