ba091b74056a4d730db9f2d83f8471f1c84ca4f6
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2018-12-04 16:27:14 +0100

    Add Timer

--- 2018/solution.hs
+++ 2018/solution.hs
@@ -1,14 +1,29 @@
-import Solutions.Day4
+import Solutions.Day2
+
+import System.CPUTime
 import System.IO
+import Text.Printf
 
 import CommonHelpers
 
 main = do
     input <- getContents
-    -- input <- readFile "Inputs/day1.txt"
     let problem = lines input
+
     putStr "\n"
+
+    start1 <- getCPUTime
     putStr $ "Deel 1: " ++ solveP1 problem
+    end1 <- getCPUTime
     putStr "\n"
+
+    start2 <- getCPUTime
     putStr $ "Deel 2: " ++ solveP2 problem
+    end2 <- getCPUTime
+    putStr "\n"
+
+    let d1 = (fromIntegral (end1 - start1)) / (10^12)
+    let d2 = (fromIntegral (end2 - start2)) / (10^12)
+
+    printf "Timings - 1: %0.3f sec, 2: %1.3f sec\n" (d1 :: Double) (d2 :: Double)
     putStr "\n"
