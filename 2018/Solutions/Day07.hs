8ad3fb6dc35a44566a8bd7141ecc6864130b6e32
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2018-12-07 11:43:45 +0100

    Day 7 working part 1 (starting rewrite of part 1)

--- 2018/Solutions/Day07.hs
+++ 2018/Solutions/Day07.hs
@@ -13,6 +13,11 @@ solvers = [solveP1,solveP2]
 parse :: String -> (String,S.Set String)
 parse x =
     let split = words x
+    in (split !! 1, S.fromList [split !! 7])
+
+parseDep :: String -> (String,S.Set String)
+parseDep x =
+    let split = words x
     in (split !! 7, S.fromList [split !! 1])
 
 firstInSet = head . S.toList
@@ -29,32 +34,31 @@ findTargets key graph = case M.lookup key graph of
 --     in if S.null newTargets then first
 --         else first ++ (recurse1 (tail newStack) (head newStack) graph)
 
+targetsToGo :: String -> M.Map String (S.Set String) -> S.Set String
+targetsToGo from graph = S.insert from $ S.foldl (\acc x -> S.union acc $ targetsToGo x graph) S.empty $ findTargets from graph
+
+firstEligibleInSet :: S.Set String -> M.Map String (S.Set String) -> String
+firstEligibleInSet targets graph =
+    let
+        blocked = S.foldl S.union S.empty $ S.map (\x -> S.delete x $ targetsToGo x graph) targets
+    in head $ filter (not . flip S.member blocked) $ S.toList targets
+
 recurse1 :: S.Set String -> M.Map String (S.Set String) -> String
 recurse1 targets graph =
     let
-        top = lastInSet targets
-        newTargets = S.delete top $ S.union targets $ findTargets top graph
+        top = firstEligibleInSet targets graph
+        newTargets = S.delete top $ S.union targets $ targetsToGo top graph
     in if S.null newTargets then top
-        else
-            let deeper = recurse1 newTargets graph
-            in if L.isInfixOf top deeper then deeper else deeper ++ top
-
--- recurse1 :: S.Set String -> String -> M.Map String (S.Set String) -> String
--- recurse1 targets top graph =
---     let newTargets = S.union targets $ findTargets top graph
---     in if S.null newTargets then top
---         else (recurse1 newTargets (lastInSet newTargets) graph) ++ top
+        else top ++ (recurse1 newTargets graph)
 
 solveP1 :: [String] -> String
 solveP1 x =
     let
         input = map parse x
-        dependencyGraph = M.fromListWith (\n1 n2 -> S.union n1 n2) $ input
-        targets = foldl (S.union) S.empty $ M.elems dependencyGraph
-        last = fst $ head $ filter (not . (flip S.member targets) . fst) input
-    in recurse1 (S.fromList [last]) dependencyGraph
-    -- in show last
-    -- in show $ lastInSet $ findTargets last dependencyGraph
+        graph = M.fromListWith (\n1 n2 -> S.union n1 n2) $ input
+        targets = foldl (S.union) S.empty $ M.elems graph
+        first = S.fromList $ L.nub $ map fst $ filter (not . (flip S.member targets) . fst) input
+    in recurse1 first graph
 
 -- || Start Part 2
 
