9351ab31c9c5af9b71534af5bf3454ca9c100b78
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2019-12-01 20:52:14 +0100

    Day 1 2019

--- 2019/src/day01.rs
+++ 2019/src/day01.rs
@@ -10,35 +10,60 @@ pub fn part2() -> u32 {
     return run2(input);
 }
 
-fn run1(_input:&str) -> u32 {
-    return 0;
+fn fuel_usage(mass: u32) -> u32 {
+  return (mass / 3) - 2;
 }
 
-fn run2(_input:&str) -> u32 {
-    return 0;
+fn run1(input:&str) -> u32 {
+  let mut fuel_need: u32 = 0;
+  for mass in str::lines(input) {
+    fuel_need += fuel_usage(mass.parse().unwrap());
+  }
+  return fuel_need;
+}
+
+fn fuel_need_r(mass: u32) -> u32 {
+  if mass < 6 { return 0; }
+  let extra_fuel = fuel_usage(mass);
+  return extra_fuel + fuel_need_r(extra_fuel);
+}
+
+fn run2(input:&str) -> u32 {
+  let mut fuel_need: u32 = 0;
+  for mass in str::lines(input) {
+    fuel_need += fuel_need_r(mass.parse().unwrap());
+  }
+  return fuel_need;
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
+  const EXAMPLE: &str = "12
+14
+1969
+100756
+";
+
   #[test]
   fn part1_sample_test() {
-    // assert_eq!(1284, part2());
+    
+    assert_eq!(34241, run1(EXAMPLE));
   }
 
   #[test]
   fn part2_sample_test() {
-    // assert_eq!(1284, part2());
+    assert_eq!(51316, run2(EXAMPLE));
   }
 
   #[test]
   fn part1_test() {
-    // assert_eq!(1284, part2());
+    assert_eq!(3405637, part1());
   }
 
   #[test]
   fn part2_test() {
-    // assert_eq!(1284, part2());
+    assert_eq!(5105597, part2());
   }
 }
