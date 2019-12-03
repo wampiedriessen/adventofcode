63590baea2b524d3ed03c35b10a9e4aa638dfc78
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2019-12-03 21:48:07 +0100

    Performance dingetje toegevoegd

--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -28,94 +28,151 @@ mod day23;
 mod day24;
 mod day25;
 
+use std::time::Instant;
 use std::env;
-use std::process::exit;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
-	if args.len() == 3 {
-		let day = args[1].parse::<u32>().expect("day is invalid");
-		let part = args[2].parse::<u32>().expect("part is invalid");
-		
-		run(day,part);
+	if args.len() == 1 {
+		for day in 1..26 {
+			run(day, 1);
+			run(day, 2);
+		}
 		return;
 	}
 
 	if args.len() == 2 {
-		let day = args[1].parse::<u32>().expect("day is invalid");
-		for i in 1..3 {
-			run(day,i);
-		}
-		return;
-	}
-
-	if args.len() == 1 {
-		for i in 1..26 {
-			for j in 1..3 {
-				run(i,j);
+		if args[1] == "perf" {
+			for day in 1..26 {
+				perf(day, 1);
+				perf(day, 2);
 			}
+			return;
 		}
+		let day = args[1].parse().unwrap();
+		run(day, 1);
+		run(day, 2);
 		return;
 	}
 
-	println!("Usage: {} day part", &args[0]);
-	exit(1);
+	if args.len() == 3 {
+		if args[1] == "perf" {
+			let day = args[2].parse().unwrap();
+			perf(day, 1);
+			perf(day, 2);
+			return;
+		}
+		let day = args[1].parse().unwrap();
+		let part = args[2].parse().unwrap();
+
+		run(day, part);
+		return;
+	}
+
+	if args.len() == 4 {
+		if args[1] != "perf" {
+			panic!("Usage is wrong");
+		}
+		let day = args[2].parse().unwrap();
+		let part = args[3].parse().unwrap();
+
+		perf(day, part);
+		return;
+	}
+
+	panic!("Usage is wrong");
 }
 
-fn run(day:u32, part:u32) {
-	match (day, part) {
-		(01, 1) => println!("{}", day01::part1()),
-		(01, 2) => println!("{}", day01::part2()),
-		(02, 1) => println!("{}", day02::part1()),
-		(02, 2) => println!("{}", day02::part2()),
-		(03, 1) => println!("{}", day03::part1()),
-		(03, 2) => println!("{}", day03::part2()),
-		(04, 1) => println!("{}", day04::part1()),
-		(04, 2) => println!("{}", day04::part2()),
-		(05, 1) => println!("{}", day05::part1()),
-		(05, 2) => println!("{}", day05::part2()),
-		(06, 1) => println!("{}", day06::part1()),
-		(06, 2) => println!("{}", day06::part2()),
-		(07, 1) => println!("{}", day07::part1()),
-		(07, 2) => println!("{}", day07::part2()),
-		(08, 1) => println!("{}", day08::part1()),
-		(08, 2) => println!("{}", day08::part2()),
-		(09, 1) => println!("{}", day09::part1()),
-		(09, 2) => println!("{}", day09::part2()),
-		(10, 1) => println!("{}", day10::part1()),
-		(10, 2) => println!("{}", day10::part2()),
-		(11, 1) => println!("{}", day11::part1()),
-		(11, 2) => println!("{}", day11::part2()),
-		(12, 1) => println!("{}", day12::part1()),
-		(12, 2) => println!("{}", day12::part2()),
-		(13, 1) => println!("{}", day13::part1()),
-		(13, 2) => println!("{}", day13::part2()),
-		(14, 1) => println!("{}", day14::part1()),
-		(14, 2) => println!("{}", day14::part2()),
-		(15, 1) => println!("{}", day15::part1()),
-		(15, 2) => println!("{}", day15::part2()),
-		(16, 1) => println!("{}", day16::part1()),
-		(16, 2) => println!("{}", day16::part2()),
-		(17, 1) => println!("{}", day17::part1()),
-		(17, 2) => println!("{}", day17::part2()),
-		(18, 1) => println!("{}", day18::part1()),
-		(18, 2) => println!("{}", day18::part2()),
-		(19, 1) => println!("{}", day19::part1()),
-		(19, 2) => println!("{}", day19::part2()),
-		(20, 1) => println!("{}", day20::part1()),
-		(20, 2) => println!("{}", day20::part2()),
-		(21, 1) => println!("{}", day21::part1()),
-		(21, 2) => println!("{}", day21::part2()),
-		(22, 1) => println!("{}", day22::part1()),
-		(22, 2) => println!("{}", day22::part2()),
-		(23, 1) => println!("{}", day23::part1()),
-		(23, 2) => println!("{}", day23::part2()),
-		(24, 1) => println!("{}", day24::part1()),
-		(24, 2) => println!("{}", day24::part2()),
-		(25, 1) => println!("{}", day25::part1()),
-		(25, 2) => println!("{}", day25::part2()),
+fn run(day: u32, part: u32) {
+	println!("{}", run_internal(day, part));
+}
 
-		_ => println!("Day {} part {} not found", day, part),
+fn run_internal(day:u32, part:u32) -> String {
+	match (day, part) {
+		(01, 1) => return format!("{}", day01::part1()),
+		(01, 2) => return format!("{}", day01::part2()),
+		(02, 1) => return format!("{}", day02::part1()),
+		(02, 2) => return format!("{}", day02::part2()),
+		(03, 1) => return format!("{}", day03::part1()),
+		(03, 2) => return format!("{}", day03::part2()),
+		(04, 1) => return format!("{}", day04::part1()),
+		(04, 2) => return format!("{}", day04::part2()),
+		(05, 1) => return format!("{}", day05::part1()),
+		(05, 2) => return format!("{}", day05::part2()),
+		(06, 1) => return format!("{}", day06::part1()),
+		(06, 2) => return format!("{}", day06::part2()),
+		(07, 1) => return format!("{}", day07::part1()),
+		(07, 2) => return format!("{}", day07::part2()),
+		(08, 1) => return format!("{}", day08::part1()),
+		(08, 2) => return format!("{}", day08::part2()),
+		(09, 1) => return format!("{}", day09::part1()),
+		(09, 2) => return format!("{}", day09::part2()),
+		(10, 1) => return format!("{}", day10::part1()),
+		(10, 2) => return format!("{}", day10::part2()),
+		(11, 1) => return format!("{}", day11::part1()),
+		(11, 2) => return format!("{}", day11::part2()),
+		(12, 1) => return format!("{}", day12::part1()),
+		(12, 2) => return format!("{}", day12::part2()),
+		(13, 1) => return format!("{}", day13::part1()),
+		(13, 2) => return format!("{}", day13::part2()),
+		(14, 1) => return format!("{}", day14::part1()),
+		(14, 2) => return format!("{}", day14::part2()),
+		(15, 1) => return format!("{}", day15::part1()),
+		(15, 2) => return format!("{}", day15::part2()),
+		(16, 1) => return format!("{}", day16::part1()),
+		(16, 2) => return format!("{}", day16::part2()),
+		(17, 1) => return format!("{}", day17::part1()),
+		(17, 2) => return format!("{}", day17::part2()),
+		(18, 1) => return format!("{}", day18::part1()),
+		(18, 2) => return format!("{}", day18::part2()),
+		(19, 1) => return format!("{}", day19::part1()),
+		(19, 2) => return format!("{}", day19::part2()),
+		(20, 1) => return format!("{}", day20::part1()),
+		(20, 2) => return format!("{}", day20::part2()),
+		(21, 1) => return format!("{}", day21::part1()),
+		(21, 2) => return format!("{}", day21::part2()),
+		(22, 1) => return format!("{}", day22::part1()),
+		(22, 2) => return format!("{}", day22::part2()),
+		(23, 1) => return format!("{}", day23::part1()),
+		(23, 2) => return format!("{}", day23::part2()),
+		(24, 1) => return format!("{}", day24::part1()),
+		(24, 2) => return format!("{}", day24::part2()),
+		(25, 1) => return format!("{}", day25::part1()),
+		(25, 2) => return format!("{}", day25::part2()),
+
+		_ => return format!("Day {} part {} not found", day, part),
 	}
+}
+
+fn perf(day: u32, part: u32) {
+	let n = 20;
+	println!("Day {} part {}. Running {} times.", day, part, n);
+
+	let mut rounds: Vec<std::time::Duration> = Vec::new();
+	for _ in 0..n {
+		let start = Instant::now();
+		run_internal(day, part);
+		rounds.push(start.elapsed());
+	}
+
+	let mean = mean(&rounds);
+	let stddev = stddeviation(mean, &rounds);
+	let error = stddev / (n as f64).sqrt();
+
+	println!("|  Puzzle  |     Mean |     Error |    StdDev |");
+	println!("|----------|----------|-----------|-----------|");
+	println!("|  D{:02} p{}  | {:>5.0} ms | {:.4} ms | {:.4} ms |", day, part, mean, error, stddev);
+}
+
+fn mean(list: &Vec<std::time::Duration>) -> f64 {
+    let sum: u128 = Iterator::sum(list.iter().map(|d| d.as_millis()));
+    f64::from(sum as u32) / (list.len() as f64)
+}
+
+fn stddeviation(mean: f64, list: &Vec<std::time::Duration>) -> f64 {
+	return list.iter()
+		.map(|v| f64::from(v.as_millis()as u32))
+		.map(|v| (v - mean)*(v - mean))
+		.sum::<f64>() / (list.len() - 1) as f64;
 }
