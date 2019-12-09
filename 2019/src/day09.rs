f19dc592676dd0fb8b66ebbcacb0ce98a605f126
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2019-12-09 11:48:23 +0100

    Completed intcode computer, finished day09

--- 2019/src/day09.rs
+++ 2019/src/day09.rs
@@ -1,13 +1,14 @@
+use super::intcode::{IntcodeProg, Intcode};
 use super::Day;
 
 pub struct Day09 {
-  _input: u32
+  input: IntcodeProg
 }
 
 impl Day for Day09 {
-  fn new(_input: &str) -> Day09 {
+  fn new(input: &str) -> Day09 {
     Day09 {
-      _input: 0,
+      input: Intcode::read_input(input),
     }
   }
 
@@ -22,11 +23,42 @@ impl Day for Day09 {
 
 // -- Privates
 impl Day09 {
-    fn run1(&self) -> i32 {
-        return 0;
-    }
+  fn run1(&self) -> i64 {
+    let mut t = Intcode::new(&self.input);
 
-    fn run2(&self) -> i32 {
-        return 0;
+    t.stdin(1);
+
+    t.compute();
+
+    return t.stdout().unwrap();
+  }
+
+    fn run2(&self) -> i64 {
+      let mut t = Intcode::new(&self.input);
+
+      t.stdin(2);
+  
+      t.compute();
+  
+      return t.stdout().unwrap();
     }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn part1() {
+    let input = include_str!("../inputs/day09.txt").trim();
+    let day = Day09::new(input);
+    assert_eq!("2204990589", format!("{}", day.part1()));
+  }
+
+  #[test]
+  fn part2() {
+    let input = include_str!("../inputs/day09.txt").trim();
+    let day = Day09::new(input);
+    assert_eq!("50008", format!("{}", day.part2()));
+  }
 }
--- 2019/src/intcode.rs
+++ 2019/src/intcode.rs
@@ -1,17 +1,19 @@
 use std::collections::VecDeque;
 
-pub type IntcodeProg = Vec<i64>;
+type IntcodeType = i64;
+pub type IntcodeProg = Vec<IntcodeType>;
 
 const FINISHED: u8 = 0;
-const WAITING_FOR_INPUT: u8 = 1;
-const RUNNING: u8 = 2;
+const RUNNING: u8 = 1;
+const WAITING_FOR_INPUT: u8 = 2;
 
 pub struct Intcode {
     pc: usize,
     ops: IntcodeProg,
-    read_queue: VecDeque<i64>,
-    write_queue: VecDeque<i64>,
+    read_queue: VecDeque<IntcodeType>,
+    write_queue: VecDeque<IntcodeType>,
     state: u8,
+    relative_base: IntcodeType,
 }
 
 impl Intcode {
@@ -22,6 +24,7 @@ impl Intcode {
             read_queue: VecDeque::new(),
             write_queue: VecDeque::new(),
             state: RUNNING,
+            relative_base: 0,
         }
     }
 
@@ -39,10 +42,11 @@ impl Intcode {
                 2   => self.op_mul(),
                 3   => self.op_cin(),
                 4   => self.op_cout(),
-                5   => self.op_jump(true),
-                6   => self.op_jump(false),
-                7   => self.op_lt(),
-                8   => self.op_eq(),
+                5   => self.op_jump(|a| a != 0),
+                6   => self.op_jump(|a| a == 0),
+                7   => self.op_cmp(|a,b| a < b),
+                8   => self.op_cmp(|a,b| a == b),
+                9   => self.op_adjust_rel_base(),
                 _   => panic!("Unknown opcode: {:?}!", op),
             }
 
@@ -52,48 +56,66 @@ impl Intcode {
     }
 
     pub fn result(&self) -> i64 {
-        return self.ops[0];
+        return self.ops[0] as i64;
     }
 
     pub fn is_finished(&self) -> bool {
         return self.state == FINISHED;
     }
 
-    fn get_param(&self, par_num: usize) -> i64 {
+    fn get_val(&mut self, pos: usize) -> IntcodeType {
+        if pos >= self.ops.len() {
+            self.ops.resize(pos+1, 0);
+            return 0;
+        }
+        return self.ops[pos];
+    }
+
+    fn get_param(&self, par_num: usize) -> usize {
         let i = 10_i64.pow((par_num+1) as u32);
         let p_mode = (self.ops[self.pc] / i) % 10;
-        let val = self.ops[self.pc+par_num];
+        let index = self.pc+par_num;
+        let val = self.ops[index];
 
         return match p_mode {
-            0 => self.ops[val as usize],
-            1 => val,
+            0 => val as usize,
+            1 => index,
+            2 => (self.relative_base + val) as usize,
             _ => panic!("Unknown parameter mode")
         }
     }
 
-    fn op_add(&mut self) {
-        let a: i64 = self.get_param(1);
-        let b: i64 = self.get_param(2);
-        let dst: i64 = self.ops[self.pc+3];
+    fn set_val(&mut self, pos: usize, val: IntcodeType) -> () {
+        // println!("p: {}, v:{}", pos, val);
+        if pos >= self.ops.len() {
+            self.ops.resize(pos+1, 0);
+        }
+        self.ops[pos] = val;
+    }
 
-        self.ops[dst as usize] = a + b;
+    fn op_add(&mut self) {
+        let a: IntcodeType = self.get_val(self.get_param(1));
+        let b: IntcodeType = self.get_val(self.get_param(2));
+        let dst: usize = self.get_param(3);
+
+        self.set_val(dst, a + b);
         self.pc += 4;
     }
 
     fn op_mul(&mut self) {
-        let a: i64 = self.get_param(1);
-        let b: i64 = self.get_param(2);
-        let dst: i64 = self.ops[self.pc+3];
+        let a: IntcodeType = self.get_val(self.get_param(1));
+        let b: IntcodeType = self.get_val(self.get_param(2));
+        let dst: usize = self.get_param(3);
 
-        self.ops[dst as usize] = a * b;
+        self.set_val(dst, a * b);
         self.pc += 4;
     }
 
     fn op_cin(&mut self) {
         if let Some(input) = self.io_read() {
-            let dst: i64 = self.ops[self.pc+1];
+            let dst: usize = self.get_param(1);
     
-            self.ops[dst as usize] = input;
+            self.set_val(dst, input);
             self.pc += 2;
             return;
         }
@@ -102,51 +124,46 @@ impl Intcode {
     }
 
     fn op_cout(&mut self) {
-        let src: i64 = self.get_param(1);
+        let src: IntcodeType = self.get_val(self.get_param(1));
 
         self.io_write(src);
         self.pc += 2;
     }
 
-    fn op_jump(&mut self, jump_if: bool) {
-        let a: i64 = self.get_param(1);
+    fn op_jump<F>(&mut self, predicate: F) -> () where F : Fn(IntcodeType) -> bool {
+        let a: IntcodeType = self.get_val(self.get_param(1));
 
-        if (a != 0) == jump_if {
-            let dst: i64 = self.get_param(2);
-            self.pc = dst as usize;
+        if predicate(a) {
+            self.pc = self.get_val(self.get_param(2)) as usize;
             return;
         }
 
         self.pc += 3;
     }
 
-    fn op_lt(&mut self) {
-        let a: i64 = self.get_param(1);
-        let b: i64 = self.get_param(2);
-        let dst: i64 = self.ops[self.pc+3];
+    fn op_cmp<F>(&mut self, predicate: F) -> () where F : Fn(IntcodeType, IntcodeType) -> bool {
+        let a: IntcodeType = self.get_val(self.get_param(1));
+        let b: IntcodeType = self.get_val(self.get_param(2));
+        let dst: usize = self.get_param(3);
 
-        self.ops[dst as usize] = match a < b {
-            true   => 1,
-            false  => 0,
-        };
+        if predicate(a,b) {
+            self.set_val(dst, 1);
+        } else {
+            self.set_val(dst, 0);
+        }
         self.pc += 4;
     }
 
-    fn op_eq(&mut self) {
-        let a: i64 = self.get_param(1);
-        let b: i64 = self.get_param(2);
-        let dst: i64 = self.ops[self.pc+3];
+    fn op_adjust_rel_base(&mut self) {
+        let a: IntcodeType = self.get_val(self.get_param(1));
 
-        self.ops[dst as usize] = match a == b {
-            true   => 1,
-            false  => 0,
-        };
-        self.pc += 4;
+        self.relative_base += a;
+        self.pc += 2;
     }
 
     // -- I/O stuff:
 
-    fn io_read(&mut self) -> Option<i64> {
+    fn io_read(&mut self) -> Option<IntcodeType> {
         if self.read_queue.is_empty() {
             return None;
         }
@@ -158,7 +175,8 @@ impl Intcode {
         self.read_queue.push_back(val);
     }
 
-    fn io_write(&mut self, val: i64) {
+    fn io_write(&mut self, val: IntcodeType) {
+        // println!("{}", val);
         self.write_queue.push_back(val);
     }
 
@@ -171,6 +189,8 @@ impl Intcode {
         println!("p: {:?}", self.ops);
         println!("r: {:?}", self.read_queue);
         println!("w: {:?}", self.write_queue);
+        println!("s: {:?}", self.state);
+        println!("b: {:?}", self.relative_base);
     }
 }
 
@@ -195,7 +215,7 @@ mod tests {
   }
 
   #[test]
-  fn jump_tests() {
+  fn jump_position_mode_tests() {
     // Position mode jump
     let prog_jump_pos = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
 
@@ -204,32 +224,36 @@ mod tests {
     t.compute();
     assert_eq!(0, t.stdout().unwrap());
 
-    let mut t = Intcode::new(&prog_jump_pos);
+    t = Intcode::new(&prog_jump_pos);
     t.stdin(-17);
     t.compute();
     assert_eq!(1, t.stdout().unwrap());
+  }
 
-
+  #[test]
+  fn jump_immediate_mode_tests() {
     // Immediate mode jump
     let prog_jump_imm = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
-    
-    t = Intcode::new(&prog_jump_imm);
+
+    let mut t = Intcode::new(&prog_jump_imm);
     t.stdin(0);
     t.compute();
     assert_eq!(0, t.stdout().unwrap());
 
-    let mut t = Intcode::new(&prog_jump_imm);
+    t = Intcode::new(&prog_jump_imm);
     t.stdin(-17);
     t.compute();
     assert_eq!(1, t.stdout().unwrap());
+  }
 
-
+  #[test]
+  fn jump_complicated_example_tests() {
     // More complicated Jump example
     let prog_jump = vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
     1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
     999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
 
-    t = Intcode::new(&prog_jump);
+    let mut t = Intcode::new(&prog_jump);
     t.stdin(1);
     t.compute();
     assert_eq!(999, t.stdout().unwrap());
@@ -243,5 +267,39 @@ mod tests {
     t.stdin(11);
     t.compute();
     assert_eq!(1001, t.stdout().unwrap());
+  }
+
+  #[test]
+  fn quine_test()
+  {
+      let prog = vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+      let mut t = Intcode::new(&prog);
+      t.compute();
+
+      let mut i = 0;
+      while let Some(x) = t.stdout() {
+        assert_eq!(prog[i], x);
+        i += 1;
+      }
+  }
+
+  #[test]
+  fn test_bigint()
+  {
+        let prog = vec![1102,34915192,34915192,7,4,7,99,0];
+        let mut t = Intcode::new(&prog);
+        t.compute();
+
+        assert_eq!(1219070632396864, t.stdout().unwrap());
+  }
+
+  #[test]
+  fn test_bigint2()
+  {
+    let prog = vec![104,1125899906842624,99];
+    let mut t = Intcode::new(&prog);
+    t.compute();
+
+    assert_eq!(1125899906842624, t.stdout().unwrap());
   }
 }
