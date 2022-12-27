7e504806d2b0b6c98f9c1055703d66bb25830e06
Wampie Driessen <1059058+wampiedriessen@users.noreply.github.com>
2022-12-27 11:43:56 +0100

    Day 3

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,56 +1,11 @@
 mod day01;
 mod day02;
-// mod day03;
-// mod day04;
-// mod day05;
-// mod day06;
-// mod day07;
-// mod day08;
-// mod day09;
-// mod day10;
-// mod day11;
-// mod day12;
-// mod day13;
-// mod day14;
-// mod day15;
-// mod day16;
-// mod day17;
-// mod day18;
-// mod day19;
-// mod day20;
-// mod day21;
-// mod day22;
-// mod day23;
-// mod day24;
-// mod day25;
-
-use crate::day01::Day01;
-use crate::day02::Day02;
-// use crate::day03::Day03;
-// use crate::day04::Day04;
-// use crate::day05::Day05;
-// use crate::day06::Day06;
-// use crate::day07::Day07;
-// use crate::day08::Day08;
-// use crate::day09::Day09;
-// use crate::day10::Day10;
-// use crate::day11::Day11;
-// use crate::day12::Day12;
-// use crate::day13::Day13;
-// use crate::day14::Day14;
-// use crate::day15::Day15;
-// use crate::day16::Day16;
-// use crate::day17::Day17;
-// use crate::day18::Day18;
-// use crate::day19::Day19;
-// use crate::day20::Day20;
-// use crate::day21::Day21;
-// use crate::day22::Day22;
-// use crate::day23::Day23;
-// use crate::day24::Day24;
-// use crate::day25::Day25;
+mod day03;
 
 use std::io::BufRead;
+use crate::day01::Day01;
+use crate::day02::Day02;
+use crate::day03::Day03;
 
 pub trait Day {
     fn part1(&self) -> String;
@@ -64,7 +19,7 @@ fn main() {
     let day: Box<dyn Day> = match args[1].as_str() {
         "1" | "01" => Box::new(Day01 { input: all_input() }),
         "2" | "02" => Box::new(Day02 { input: all_input() }),
-        // "3" | "03" => Box::new(Day03 { input: all_input() }),
+        "3" | "03" => Box::new(Day03 { input: all_input() }),
         // "4" | "04" => Box::new(Day04 { input: all_input() }),
         // "5" | "05" => Box::new(Day05 { input: all_input() }),
         // "6" | "06" => Box::new(Day06 { input: all_input() }),
