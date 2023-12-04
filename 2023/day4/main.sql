b5554b82239b3dbc293ec7c466a073c7c9af150c
Wampie Driessen <1059058+wampiedriessen@users.noreply.github.com>
2023-12-04 13:44:36 +0100

    Finished Day4, thanks for 'with recursive' @uvanderwerf

--- 2023/day4/main.sql
+++ 2023/day4/main.sql
@@ -1,42 +1,26 @@
--- CREATE TABLE solution (
---     part varchar,
---     answer varchar
--- );
-
-CREATE TEMPORARY TABLE cards (
+CREATE TEMPORARY TABLE scratchcards (
     cardnum int,
     winning int[],
     mine int[],
-    winning_numbers int,
-    points int,
-    extra_copies int
+    number_won int default 0,
+    points int default 0
 );
 
-CREATE FUNCTION array_intersect(anyarray, anyarray)
-  RETURNS anyarray
-  language sql
-as $FUNCTION$
-    SELECT ARRAY(
-        SELECT UNNEST($1)
-        INTERSECT
-        SELECT UNNEST($2)
-    );
-$FUNCTION$;
-
-INSERT INTO cards (cardnum, winning, mine, winning_numbers, points, extra_copies) 
+INSERT INTO scratchcards (cardnum, winning, mine) 
     SELECT
         split_part(split_part(scratchcard, ':', 1), ' ', -1)::int,
-        array_remove(string_to_array(split_part(split_part(scratchcard, ':', 2), '|', 1), ' '), '')::int[],
-        array_remove(string_to_array(split_part(split_part(scratchcard, ':', 2), '|', 2), ' '), '')::int[],
-        0, 0, 0
+        array_remove(string_to_array(split_part(split_part(scratchcard, ':', 2), '|', 1), ' '), '')::int[] as winning,
+        array_remove(string_to_array(split_part(split_part(scratchcard, ':', 2), '|', 2), ' '), '')::int[] as mine
     FROM input;
 
-UPDATE cards SET winning_numbers = cardinality( array_intersect(winning, mine) );
-UPDATE cards SET points = POWER(2, winning_numbers - 1) WHERE winning_numbers > 0;
+update scratchcards set number_won = cardinality( ARRAY(select unnest(winning) INTERSECT select unnest(mine)) );
+update scratchcards set points = CASE WHEN number_won > 0 THEN POWER(2, number_won - 1) ELSE 0 END;
 
--- update cards c1 SET c1.extra_copies = (select sum(c2.extra_copies) from cards c2 where (c2.cardnum + c2.winning_numbers) >= c1.cardnum);
+INSERT INTO solution (part, answer) SELECT '1', sum(points) FROM scratchcards;
 
-
-INSERT INTO solution (part, answer) SELECT '1', sum(points) FROM cards;
-INSERT INTO solution (part, answer) SELECT '2', sum(extra_copies+1) FROM cards;
-
+with recursive r_scratch as (
+    select * from scratchcards
+    UNION ALL
+        select newcard.* from scratchcards newcard
+            join r_scratch winner on newcard.cardnum <= (winner.cardnum + winner.number_won) and newcard.cardnum > winner.cardnum
+) INSERT INTO solution (part, answer) SELECT '2', count(*) FROM r_scratch;
