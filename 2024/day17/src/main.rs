c8c5a2692e96c179d8accdfbea1069f1e216a8da
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2024-12-20 00:12:58 +0100

    2024 - Finished day 17, hacky hacky

--- 2024/day17/src/main.rs
+++ 2024/day17/src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 use std::io::Read;
 
+fn output(vec: &Vec<u8>) -> String {
+    vec.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(",")
+}
+
 fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
     let mut registers = [0; 3];
     let mut instructions = Vec::new();
-
-    let mut orig_instructions = String::new();
 
     for line in input.lines() {
         if line.starts_with("Register A: ") {
@@ -24,22 +26,24 @@ fn main() -> io::Result<()> {
         if line.starts_with("Program: ") {
             for char in line[9..].split(',') {
                 instructions.push(char.as_bytes()[0]-'0' as u8);
-
-                if !orig_instructions.is_empty() { orig_instructions += ","; }
-                orig_instructions += char;
             }
         }
     }
 
-    let mut new_program = run(&instructions, registers.clone(), &String::new(), false);
-    println!("Part 1: {}", new_program);
+    let mut new_program = run(&instructions, registers.clone());
+    println!("Part 1: {}", output(&new_program));
 
-    let mut a_register = 0;
-    while new_program != orig_instructions {
-        registers[0] = a_register;
-        a_register += 1;
+    let mut a_register = 1;
+    loop {
+        loop {
+            registers[0] = a_register;
+            new_program = run(&instructions, registers.clone());
 
-        new_program = run(&instructions, registers.clone(), &orig_instructions, true);
+            if instructions.ends_with(&new_program) { break; }
+            a_register += 1;
+        }
+        if new_program.eq(&instructions) { break; }
+        a_register <<= 3;
     }
 
     println!("Part 2: {}", a_register);
@@ -56,17 +60,13 @@ const OUT: u8 = 5;
 const BDV: u8 = 6;
 const CDV: u8 = 7;
 
-fn run(instructions: &Vec<u8>, mut registers: [i64; 3], targetstring: &String, must_reach_target: bool) -> String {
+fn run(instructions: &Vec<u8>, mut registers: [i64; 3]) -> Vec<u8> {
     let mut ip = 0;
 
-    let mut stdout = String::new();
+    let mut stdout = Vec::new();
 
     loop {
-        if must_reach_target && !targetstring.starts_with(&stdout){ return String::new(); }
-        // if loopdetector.contains(&(ip, registers[0], registers[1], registers[2], stdout.clone())) { return String::new(); }
         if ip+1 >= instructions.len() { break; }
-
-        // loopdetector.insert((ip, registers[0], registers[1], registers[2], stdout.clone()));
 
         let ins = instructions[ip];
         let op = instructions[ip+1];
@@ -77,7 +77,7 @@ fn run(instructions: &Vec<u8>, mut registers: [i64; 3], targetstring: &String, m
             BST => registers[1] = combo(&registers, op) % 8,
             JNZ => if registers[0] != 0 { ip = op as usize; continue; }
             BXC => registers[1] ^= registers[2],
-            OUT => { if !stdout.is_empty() { stdout += ","; } stdout += &(combo(&registers, op) % 8).to_string() },
+            OUT => stdout.push(combo(&registers, op) as u8 % 8),
             BDV => registers[1] = registers[0] / 2i64.pow(combo(&registers, op) as u32),
             CDV => registers[2] = registers[0] / 2i64.pow(combo(&registers, op) as u32),
             _ => panic!("Unknown instruction: {}", ins),
